0d43d045be7a11d254cf8a00a5b5fd867a0b5f9b
Matthew Hui <mh@berkeley.edu>
2015-09-14 03:03:08 -0700

    Update ffmpeg_options.rb

--- lib/carrierwave/video/ffmpeg_options.rb
+++ lib/carrierwave/video/ffmpeg_options.rb
@@ -81,7 +81,7 @@ module CarrierWave
             case format
             when 'mp4'
               h[:video_codec] = 'libx264'
-              h[:audio_codec] = 'libfaac'
+              h[:audio_codec] = 'libfdk_aac'
               h[:custom] = '-qscale 0 -preset slow -g 30'
             when 'ogv'
               h[:video_codec] = 'libtheora'
